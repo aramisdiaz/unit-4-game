075c9f6343db376e63bc961054cc4582c3454d30
Aramis Diaz <aramisdiaz@mail.usf.edu>
2020-05-03 12:16:26 -0700

    Game version v1.1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// global variables.
+
 var attack;
 var defend;
 var attackcharacter;
@@ -23,26 +23,21 @@ $("#picRow").show();
 $(".restart").hide();
 $(".attackButton").show();
 
-// reset myChar and myDef to equal nothing.
 var myChar = "";
 var myDef = "";
-// var YourCharacter;
-// var YourDefender;
 
 
-// reset health points.
+
 characters.Revan.healthPoints = 120;
 characters.Bastila.healthPoints = 150;
 characters.Malak.healthPoints = 180;
 characters.Calo.healthPoints = 100;
 
-// reset attack power.
 characters.Revan.attackPower = 8;
 characters.Bastila.attackPower = 10;
 characters.Malak.attackPower = 12;
 characters.Calo.attackPower = 10;
 
-// delete all in-game text.
 $(".youAttacked").empty();
 $(".attackedBack").empty();
 $(".youDefeated").empty();
@@ -50,31 +45,26 @@ $(".youWon").empty();
 $(".youLose").empty();
 $(".noEnemy").empty();
 
-//writing each characters full name to the html so they show up on the page.
 $(".namer").html(characters.Revan.fullName);
 $(".nameb").html(characters.Bastila.fullName);
 $(".namem").html(characters.Malak.fullName);
 $(".namec").html(characters.Calo.fullName);
 
-//adding the picture for each character so they show up on the page.
 $("#rev").appendTo("#picRow");
 $("#bast").appendTo("#picRow");
 $("#mala").appendTo("#picRow");
 $("#caloN").appendTo("#picRow");
 
-//writing each characters healthpoints to the html so they show up on the page.
 $(".Revanhp").html(characters.Revan.healthPoints);
 $(".Bastilahp").html(characters.Bastila.healthPoints);
 $(".Malakhp").html(characters.Malak.healthPoints);
 $(".Calohp").html(characters.Calo.healthPoints);
 
-// reset border colors. 
 $(".firstRow").css({"background-color": "black", "outline-color": "yellow", 
 "border-width": "3px", "outline-style": "solid", "border-color": "yellow", "outline-width": "3px"});
 
 };
 
-// array to hold each characters stats.
 var characters = { 
 
 Revan: {
@@ -118,19 +108,14 @@ Calo:{
 $(document).ready(function(){
 reset();
 
-// When the player clicks on any of the characters, the game determines which one was clicked, moves the one clicked into
-// "Your Character" and moves the other three into "Enemies available to attach".
 $(".firstRow").click(function(){
         
    if (myChar == "") {
-   // appends the chosen character to "Your Character"
    console.log(this);	       
    $(this).appendTo("#yourChar");
    myChar = $(this);
    YourCharacter = $(myChar).attr("value");
       }
-   // if else statements that determine who is currently "Your Character" and assign
-   // that person to the character array's properties. 
    if (YourCharacter == characters.Revan.name) {
            attackerHP = characters.Revan.healthPoints - battleDmg;
            attackerAP = characters.Revan.attackPower;
@@ -164,39 +149,29 @@ $(".firstRow").click(function(){
            attack = characters.Calo;
    }
           
-   // clones the three remaining characters to "Enemies available to attack" three separate divs.
    for (var i = 0; i < 4; i++) {
        $("._" + [i]).not(myChar).appendTo("#enemies" + [i]);
 
-       // changing color
        $("._" + [i]).not(myChar).css({"background-color": "red", "max-width": "100%", "outline-color": "black", 
            "border-width": "3px", "outline-style": "solid", "border-color": "black", "outline-width": "1px"});
 
 
    }
            
-   // Clears the characters from the top.
-   // $("#picRow").empty();
    $("#picRow").hide();
   
 });
 
-// When the player clicks on any of the characters in the "enemies available to attack" section, the game 
-// determines which one was clicked and moves the one clicked into the "Defender" position. The other two 
-// characters remain in "enemies available to attack" section.     
+   
 $(".move").click(function(){
 
-     // if (myDef === "") {
-        // clones the chosen character to "Defender"
-     // moves that character to the "Defender" section on the page.
+    
      $(this).appendTo("#defender");
      myDef = $(this);
      YourDefender = $(myDef).children().attr("value");
      $(".youDefeated").empty();
 
- // }
-     // if else statements that determine who is currently "Defender" and assign
-   // that person to the character array's properties.
+
    if (YourDefender == characters.Revan.name) {
 
            defenderHP = characters.Revan.healthPoints;
@@ -237,62 +212,49 @@ $(".move").click(function(){
 });
 
 
-// when the user clicks attack, the player/Your Character's Health Points go down based on the counter attack 
-// property of the "Defender".Their counter attack decreases your health.
+
 $(".attackButton").click(function(){
 
-     // if player clicks attack button and no one is in the "defender" div, then 
-     // game says "no enemy here".
+
      if ($("#defender").children().length == 0) {
          $(".noEnemy").html("No enemy here.");
      }
 
      if (!(attackerHP < 1) || !(defenderHP < 1)) {
 
-     // when button is clicked (if both players healthpoints are not 0, 
-     // the game subtracts the defendersCAP from the attackers HP.)
+
      attackerHP = (attackerHP - defenderCAP);
      
-     // writing the attacker/Your Character's new healthpoints to the html. 
+ 
      battleDmg = battleDmg + defenderCAP;
     
     $("." + YourCharacter).html(attackerHP);
 
  
-     // writing the text "You attacked Bastila Shan for 8 damage".
      $(".youAttacked").html("You attacked " + defenderFN + " for " + attackerAP + " damage.");
 
-     // when button is clicked (if both players healthpoints are not 0, 
-     // the game subtracts the attackers AP points from the defenders HP.)
      defenderHP = (defenderHP - attackerAP);
 
-     // writing the text "Bastila Shan attacked you back for 10 damage."
      $(".attackedBack").html(defenderFN + " attacked you back for " + defenderCAP + " damage.");
 
 
-    
-    // write the defender's new healthpoints to the html.
     $("." + YourDefender).html(defenderHP);
 
 
-    // Your Character's attack power goes up by AP. 
     console.log(attackerAP);
     attackerAP = (attackerAP + origAP);
     
-    // redefining "YourCharacter"'s attack power to equal the new value. 
     attack.attackPower = attackerAP;
     console.log(attackerAP);
  
 
  } 
-     // if your character defeats the defender.
+ 
      if (defenderHP <= 0) {
 
-         // clear text from the bottom and add defeated text.
          $(".youAttacked").empty();
          $(".attackedBack").empty();
          $(".youDefeated").html("You have defeated " + defenderFN + ", you can choose to fight another enemy.");
-         // remove defender from the page.
          $("#defender").empty();
 
 
@@ -300,42 +262,33 @@ $(".attackButton").click(function(){
 
      }
      
-     // if all enemies have been defeated and the attacker still has health, then the player wins
      if ($(".move").children().length == 0){
       
-      // clear out the other paragraphs and let user know they won.
       $(".youAttacked").empty();
       $(".attackedBack").empty();
       $(".youDefeated").empty();
       $(".noEnemy").empty();
       $(".youWon").html("You Won!!!! GAME OVER!!!"); 
 
-      // show the restart button. 
       $(".restart").show();
 
-      // When you click "Restart" the game begins again. 
       $(".restart").click(function(){
           location.reload(true);
       })
                        
      }
 
-     // if your characters hp = 0 then you lose.
      if (attackerHP <= 0) {
 
-         // show the restart button.
          $(".restart").show();
 
-         // hide the attack button.
          $(".attackButton").hide();
 
-         // You lose.
          $(".youAttacked").empty();
           $(".attackedBack").empty();
          $(".youDefeated").empty();
          $(".youLose").html("You've been defeated...GAME OVER!!!")
 
-          // When you click "Restart" the game begins again. 
           $(".restart").click(function(){
               location.reload(true);
           });
@@ -345,5 +298,4 @@ $(".attackButton").click(function(){
 });
 
 
-// The game remembers every time you attack and slowly increases your attack power. 
 });
